aefd634c7ced9a3066ac3bd50418eac110a5d186
David Torres <davidatorres@cpp.edu>
2023-11-16 22:00:52 -0800

    finished code with exported data txt files

--- Main.java
+++ Main.java
@@ -1,14 +1,30 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double a = 0;
-        double b = 1;
-        double c = b - (functionOne(b)/functionOneDer(b)); //debug code
-        double fOfC = functionOne(c);
-        System.out.printf("C value = %f: \nf(c) = %f: \n", c , fOfC);
-        //bisectionMethod(a,b); //WORKS!!!!!!
-        //falsePositionMethod(a, b); //WORKS!!!!!
-        //newtonRaphsonMethod(a,b); //WORKS!!!!!
-        //secantMethod(a,b); //WORKS!!!!!
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please choose the first starting value for the methods to use on function 1: ");
+        double a = Double.parseDouble(scanner.nextLine());
+        System.out.println("Please choose the second starting value for the methods to use on function 1: ");
+        double b = Double.parseDouble(scanner.nextLine());
+
+        bisectionMethod(a,b);
+        falsePositionMethod(a, b);
+        newtonRaphsonMethod(a,b);
+        secantMethod(a,b);
+
+        System.out.println("Please choose the first starting value for the methods to use on function 2: ");
+        double c = Double.parseDouble(scanner.nextLine());
+        System.out.println("Please choose the second starting value for the methods to use on function 2: ");
+        double d = Double.parseDouble(scanner.nextLine());
+
+        bisectionMethodF2(c,d);
+        falsePositionMethodF2(c, d);
+        newtonRaphsonMethodF2(c,d);
+        secantMethodF2(c,d);
     }
 
     public static double[] bisectionMethod(double a, double b) {
@@ -36,6 +52,20 @@ public class Main {
 
         if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
                 "percent error within 100 iterations. ");
+
+        // export code
+        String filePath = "bisectFunction1_R34.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return relativePercentError;
     }
@@ -66,6 +96,19 @@ public class Main {
         if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
                 "percent error within 100 iterations. ");
 
+        // export code
+        String filePath = "falsePosFunction1_R34.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return relativePercentError;
     }
 
@@ -77,13 +120,27 @@ public class Main {
             relativePercentError[i] = relErrorPercent(c, b);
             if (relativePercentError[i] < 1) {
                 errorGoalReached = true;
-                return relativePercentError;
+                break;
             }
             b = c;
             c = b - (functionOne(b)/functionOneDer(b));
         }
         if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
                 "percent error within 100 iterations. ");
+
+        // export code
+        String filePath = "newtonRaFunction1_R34.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return relativePercentError;
     }
@@ -113,8 +170,187 @@ public class Main {
         if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
                 "percent error within 100 iterations. ");
 
+        // export code
+        String filePath = "secantFunction1_R34.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return relativePercentError;
     }
+
+    //**************************************************** CODE FOR FUNCTION 2 **************************************
+    //***************************************************************************************************************
+    public static double[] bisectionMethodF2(double a, double b) {
+        boolean errorGoalReached = false;
+        double [] relativePercentError = new double[100];
+        double fOfA = functionTwo(a);
+        double fOfB = functionTwo(b);
+        double c = (a+b)/2;
+        double cOldVal;
+        double fOfC = functionTwo(c);
+
+        for (int i = 0; i < 100; i++) {
+            if ((fOfC < 0 && fOfA < 0) || (fOfC > 0 && fOfA > 0)) a = c;
+            else b = c;
+            cOldVal = c;
+            c = (a+b)/2;
+            fOfC = functionTwo(c);
+
+            relativePercentError[i] = relErrorPercent(c, cOldVal);
+            if (relativePercentError[i] < 1) {
+                errorGoalReached = true;
+                break;
+            }
+        }
+
+        if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
+                "percent error within 100 iterations. ");
+
+        // export code
+        String filePath = "bisectFunction2.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return relativePercentError;
+    }
+
+    public static double[] falsePositionMethodF2(double a, double b) {
+        boolean errorGoalReached = false;
+        double [] relativePercentError = new double[100];
+        double fOfA = functionTwo(a);
+        double fOfB = functionTwo(b);
+        double c = (a * functionTwo(b) - b * functionTwo(a)) / (functionTwo(b) - functionTwo(a));
+        double cOldVal;
+        double fOfC = functionTwo(c);
+
+        for (int i = 0; i < 100; i++) {
+            if ((fOfC < 0 && fOfA < 0) || (fOfC > 0 && fOfA > 0)) a = c;
+            else b = c;
+            cOldVal = c;
+            c = (a * functionTwo(b) - b * functionTwo(a)) / (functionTwo(b) - functionTwo(a));
+            fOfC = functionTwo(c);
+
+            relativePercentError[i] = relErrorPercent(c, cOldVal);
+            if (relativePercentError[i] < 1) {
+                errorGoalReached = true;
+                break;
+            }
+        }
+
+        if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
+                "percent error within 100 iterations. ");
+
+        // export code
+        String filePath = "falsePosFunction2.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return relativePercentError;
+    }
+
+    public static double[] newtonRaphsonMethodF2(double a, double b) { //still need to debug this method
+        boolean errorGoalReached = false;
+        double [] relativePercentError = new double[100];
+        double c = b - (functionTwo(b)/functionTwoDer(b));
+        for (int i = 0; i < 100 ; i++) {
+            relativePercentError[i] = relErrorPercent(c, b);
+            if (relativePercentError[i] < 1) {
+                errorGoalReached = true;
+                break;
+            }
+            b = c;
+            c = b - (functionTwo(b)/functionTwoDer(b));
+        }
+        if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
+                "percent error within 100 iterations. ");
+
+        // export code
+        String filePath = "newtonRaFunction2.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return relativePercentError;
+    }
+    public static double[] secantMethodF2(double a, double b) {
+        boolean errorGoalReached = false;
+        double [] relativePercentError = new double[100];
+        double fOfA = functionTwo(a);
+        double fOfB = functionTwo(b);
+        double c = b - functionTwo(b) * ((b - a)/(functionTwo(b) - functionTwo(a))); //Secant method
+        double cOldVal;
+        double fOfC = functionTwo(c);
+
+        for (int i = 0; i < 100; i++) {
+            if ((fOfC < 0 && fOfA < 0) || (fOfC > 0 && fOfA > 0)) a = c;
+            else b = c;
+            cOldVal = c;
+            c = b - functionTwo(b) * ((b - a)/(functionTwo(b) - functionTwo(a))); //Secant method
+            fOfC = functionTwo(c);
+
+            relativePercentError[i] = relErrorPercent(c, cOldVal);
+            if (relativePercentError[i] < 1) {
+                errorGoalReached = true;
+                break;
+            }
+        }
+
+        if (errorGoalReached == false) System.out.println("Sorry solution did not converge to desired relative " +
+                "percent error within 100 iterations. ");
+
+        // export code
+        String filePath = "secantFunction2.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            exportArrayToFile(relativePercentError, printWriter);
+            printWriter.close();
+            fileWriter.close();
+
+            System.out.println("Relative error array exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return relativePercentError;
+    }
+    //***************************************************************************************************************
+    //***************************************************************************************************************
 
     public static double functionOne (double a) {
         return 2*Math.pow(a, 3) - 11.7*Math.pow(a,2) + 17.7*a -5;
@@ -132,59 +368,10 @@ public class Main {
     public static double relErrorPercent(double a, double b) {
         return Math.abs(a-b)/Math.abs(a) * 100;
     }
-}
 
-/*
-* *********************************************************************CHAT GPT CODE
-* public class BisectionMethod {
-
-    // Define the function for which roots need to be found
-    public static double function(double x) {
-        // Replace this with your actual function
-        return x * x - 4;
-    }
-
-    // Bisection method implementation
-    public static double bisectionMethod(double a, double b, double tolerance) {
-        if (function(a) * function(b) >= 0) {
-            throw new IllegalArgumentException("The function values at the provided points must have different signs.");
+    public static void exportArrayToFile(double[] array, PrintWriter printWriter) {
+        for (double value : array) {
+            printWriter.println(value);
         }
-
-        double c;
-
-        do {
-            // Find the midpoint
-            c = (a + b) / 2;
-
-            // Check if the root is found or if the interval is small enough
-            if (Math.abs(function(c)) < tolerance) {
-                break;
-            }
-
-            // Update the interval based on the sign of the function at the midpoint
-            if (function(c) * function(a) < 0) {
-                b = c;
-            } else {
-                a = c;
-            }
-
-        } while (b - a > tolerance);
-
-        return c;
-    }
-
-    public static void main(String[] args) {
-        // Example usage
-        double a = 0; // Left endpoint of the interval
-        double b = 2; // Right endpoint of the interval
-        double tolerance = 0.0001; // Tolerance or acceptable error
-
-        double root = bisectionMethod(a, b, tolerance);
-
-        System.out.println("Approximate root: " + root);
     }
 }
-
-* */
-
-
